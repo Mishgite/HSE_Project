853a4f6808fa58916c6fbd3b4828531f837d3b21
Mishgite <7594201@gmail.com>
2024-10-24 21:04:37 +0300

    launch schedule

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
+import matplotlib.pyplot as plt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+name_company = {}
+with open("Space_Corrected.csv", encoding='utf-8') as r_file:
+    file_reader = csv.reader(r_file, delimiter=",")
+    count = 0
+    for row in file_reader:
+        if count != 0:
+            if row[2] in name_company.keys():
+                name_company[row[2]] += 1
+            else:
+                name_company[row[2]] = 1
+        count += 1
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+sorted_data = dict(sorted(name_company.items(), key=lambda item: item[1], reverse=True))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.bar(sorted_data.keys(), sorted_data.values())
+
+for i, v in enumerate(sorted_data.values()):
+  plt.text(i, v, str(v), ha='center', va='bottom', fontsize=8)
+
+plt.xticks(rotation=80)
+
+plt.xlabel('Организации')
+plt.ylabel('Количество запусков')
+
+plt.tight_layout()
+
+plt.show()
