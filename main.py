07100e453b44d2802e9a101f80a3e0a7e9748d10
Mishgite <7594201@gmail.com>
2024-10-25 19:14:30 +0300

    пред финал

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 
 name_company = {}
+name_country = {}
 with open("Space_Corrected.csv", encoding='utf-8') as r_file:
     file_reader = csv.reader(r_file, delimiter=",")
     count = 0
@@ -12,19 +13,40 @@ with open("Space_Corrected.csv", encoding='utf-8') as r_file:
                 name_company[row[2]] += 1
             else:
                 name_company[row[2]] = 1
+            if row[9] in name_country.keys():
+                name_country[row[9]] += 1
+            else:
+                name_country[row[9]] = 1
         count += 1
 
 
 sorted_data = dict(sorted(name_company.items(), key=lambda item: item[1], reverse=True))
+sorted_country = dict(sorted(name_country.items(), key=lambda item: item[1], reverse=True))
 
-plt.bar(sorted_data.keys(), sorted_data.values())
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(211)
+ax1.bar(sorted_data.keys(), sorted_data.values())
 
 for i, v in enumerate(sorted_data.values()):
-  plt.text(i, v, str(v), ha='center', va='bottom', fontsize=8)
+  ax1.text(i, v, str(v), ha='center', va='bottom', fontsize=8)
 
 plt.xticks(rotation=80)
 
 plt.xlabel('Организации')
+plt.ylabel('Количество запусков')
+
+
+ax2 = fig.add_subplot(212)
+ax2.bar(sorted_country.keys(), sorted_country.values())
+
+for i, v in enumerate(sorted_country.values()):
+  ax2.text(i, v, str(v), ha='center', va='bottom', fontsize=8)
+
+plt.xticks(rotation=80)
+
+plt.xlabel('Страны')
 plt.ylabel('Количество запусков')
 
 plt.tight_layout()
